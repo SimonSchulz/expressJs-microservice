68658b2c4b0cbcd85664abd783969816ba1dc100
Dair <dairkhandossayev@gmail.com>
2022-10-04 14:12:08 +0600

    created functionality

---
+++ src/user/settings/notifications/notifications.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import UserService from '../../user.service';
+import TokenController from '../../../token/token.controller';
+import { StatusCodes } from 'http-status-codes';
+import messages from '../../../utils/helpers/messages';
+
+class NotificationsController {
+  constructor(private userService: UserService, private tokenController: TokenController) {
+    this.userService = new UserService();
+    this.tokenController = new TokenController(this.userService);
+  }
+
+  public patchSmsNotifications = async (req: Request, res: Response) => {
+    try {
+      const { clientId, notificationStatus } = req.body;
+      const { authorization } = req.headers;
+      const user = await this.userService.getUser({ clientId });
+      const token = authorization.split(' ')[1];
+      const userData = await this.tokenController.validateAccessToken(token, res);
+
+      if (!user) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: messages.USER_DOESNT_EXIST });
+      } else if (!userData) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ msg: messages.USER_NOT_AUTHORIZED });
+      } else {
+        await this.userService.updateUser(clientId, { smsNotification: notificationStatus });
+      }
+    } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: messages.INTERNAL_SERVER_ERROR });
+    }
+  };
+}
+
+export default NotificationsController;
---
+++ src/user/settings/notifications/notifications.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import TokenController from '../../../token/token.controller';
+import UserService from '../../user.service';
+import NotificationsController from './notifications.controller';
+
+class NotificationsRoutes {
+  public router = Router();
+
+  private notificationsController: NotificationsController;
+
+  private userService: UserService;
+
+  private tokenController: TokenController;
+
+  constructor() {
+    this.notificationsController = new NotificationsController(this.userService, this.tokenController);
+    this.initRoutes();
+  }
+
+  private initRoutes() {
+    this.router.patch('/auth/user/settings/notifications/sms/', this.notificationsController.patchSmsNotifications);
+  }
+}
+
+export default NotificationsRoutes;
