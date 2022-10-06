034f505cb2707a076efd57a51ee9c3f495b958b6
ivanlyutov17 <lyutov17@yandex.ru>
2022-10-06 14:47:30 +0200

    resolved issues

--- src/user/settings/userSettings.controller.ts
+++ src/user/settings/userSettings.controller.ts
@@ -11,65 +11,49 @@ class UserSettingsController {
   }
 
   public changeUserSettings = async (req: Request, res: Response) => {
-    try {
-      const updateData = req.body;
-      const errorMessages = [];
-      const user = await this.userService.getUser(updateData.clientId);
+    const updateData = req.body;
+    const errorMessages = [];
+    const user = await this.userService.getUser(updateData.clientId);
 
-      if (updateData.newEmail) {
-        if (updateData.newEmail !== user.email) {
-          await this.userService.updateUserData(updateData.clientId, { email: updateData.newEmail });
-        } else {
-          errorMessages.push(messages.SAME_EMAIL);
-        }
-      }
-      if (updateData.newPassword) {
-        const passCheck = await this.userService.checkUserPassword(user, updateData.password);
-        const newHashPass = await this.userService.genHashPassword(updateData.newPassword);
-        const newPassCheck = await this.userService.checkUserPassword(user, newHashPass);
-        if (passCheck && !newPassCheck) {
-          await this.userService.updateUserData(updateData.clientId, { password: newHashPass });
-        } else {
-          errorMessages.push(messages.INVALID_PASSWORD);
-        }
-      }
-      if (updateData.securityQuestionAnswer) {
-        const securityQuestionCheck = await this.userService.checkSecQuestionData(updateData);
-        if (securityQuestionCheck) {
-          if (updateData.securityQuestionId) {
-            await this.userService.updateUserData(updateData.clientId, {
-              securityQuestionId: updateData.securityQuestionId,
-              securityQuestion: null,
-              securityQuestionType: updateData.securityQuestionType,
-              securityQuestionAnswer: await this.userService.genHashPassword(updateData.securityQuestionAnswer),
-            });
-          } else {
-            await this.userService.updateUserData(updateData.clientId, {
-              securityQuestion: updateData.securityQuestion,
-              securityQuestionId: null,
-              securityQuestionType: updateData.securityQuestionType,
-              securityQuestionAnswer: await this.userService.genHashPassword(updateData.securityQuestionAnswer),
-            });
-          }
-        } else {
-          errorMessages.push(messages.INVALID_SECURITY_DATA);
-        }
-      }
-      if (updateData.mobilePhone) {
-        if (updateData.mobilePhone !== user.mobilePhone) {
-          await this.userService.updateUserData(updateData.clientId, { mobilePhone: updateData.mobilePhone });
-        } else {
-          errorMessages.push(messages.SAME_PHONE);
-        }
-      }
-
-      if (errorMessages.length > 0) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ errorMessages: errorMessages });
+    if (updateData.newEmail && updateData.newEmail === user.email) {
+      errorMessages.push(messages.SAME_EMAIL);
+    }
+    if (updateData.newPassword) {
+      const passCheck = await this.userService.checkUserPassword(user, updateData.password);
+      const newHashPass = await this.userService.genHashPassword(updateData.newPassword);
+      const newPassCheck = await this.userService.checkUserPassword(user, newHashPass);
+      if (passCheck && !newPassCheck) {
+        updateData.newPassword = newHashPass;
       } else {
-        return res.status(StatusCodes.OK).json({ msg: messages.SUCCESS });
+        errorMessages.push(messages.INVALID_PASSWORD);
       }
-    } catch (error) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: messages.ERROR });
+    }
+    if (updateData.securityQuestionAnswer) {
+      const securityQuestionCheck = await this.userService.checkSecQuestionData(updateData);
+      if (securityQuestionCheck) {
+        updateData.securityQuestionAnswer = await this.userService.genHashPassword(updateData.securityQuestionAnswer);
+        if (updateData.securityQuestionId) {
+          updateData.securityQuestion = null;
+        } else {
+          updateData.securityQuestionId = null;
+        }
+      } else {
+        errorMessages.push(messages.INVALID_SECURITY_DATA);
+      }
+    }
+    if (updateData.mobilePhone && updateData.mobilePhone === user.mobilePhone) {
+      errorMessages.push(messages.SAME_PHONE);
+    }
+
+    if (errorMessages.length > 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ errorMessages: errorMessages });
+    } else {
+      try {
+        await this.userService.updateUserData(updateData.clientId, updateData);
+        return res.status(StatusCodes.OK).json({ msg: messages.SUCCESS });
+      } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: messages.ERROR });
+      }
     }
   };
 }
