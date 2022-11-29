adb95783b2006169d5a47527e82115ac3856a7b1
Aleksey Budanov <a.budanov@mageworx.com>
2022-11-29 11:21:25 +0100

    feature update password

--- src/security/security.controller.ts
+++ src/security/security.controller.ts
@@ -8,6 +8,7 @@ import SecurityService from './security.service';
 import { messages } from '../utils/helpers/messages';
 import generateTime from '../utils/helpers/generateTime';
 import { EmailDto } from '../registration/dto/email.dto';
+import { PassportIdDto } from '../registration/dto/passportId.dto';
 import { ClientStatus } from '../utils/helpers/ClientStatus';
 
 export default class SecurityController {
@@ -16,6 +17,73 @@ export default class SecurityController {
     this.userService = new UserService();
   }
 
+  public updatePassword = async (req: Request, res: Response) => {
+    try {
+      const { id, newPassword } = req.body;
+      const verifData = await this.securityService.getVerifDataByParam({ id });
+
+      if (!verifData) return res.status(StatusCodes.NOT_FOUND);
+      if (verifData.clientVerifStatus === ClientVerifStatus.BLOCKED) {
+        return res.status(StatusCodes.NOT_ACCEPTABLE).json({ msg: messages.CLIENT_STILL_BLOCKED });
+      }
+      const email = verifData.email;
+      const newPasswordClientData = {
+        password: newPassword,
+      };
+
+      await this.securityService.removeVerifRecord({ id });
+
+      await this.securityService.updateUserByParam({ email }, newPasswordClientData);
+
+      return res.status(StatusCodes.OK).json({ msg: messages.SUCCESS });
+    } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: messages.INTERNAL_SERVER_ERROR });
+    }
+  }
+
+  public sendVerificationCodeUpdatePassword = async (req: Request, res: Response) => {
+    try {
+      const { passportId } = plainToInstance(PassportIdDto, req.body);
+      const user = await this.userService.getUser({ passportId });
+      if(user && user.clientStatus === ClientStatus.REGISTERED) {
+        const email = user.email;
+        const verificationData = await this.securityService.getVerifDataByParam({ email });
+        if (!verificationData) {
+          const timeObj = generateTime();
+          const data = await this.securityService.sendCode(
+            email,
+            timeObj.codeExpirationTime,
+            timeObj.lastSentEmailTime
+          );
+          const { id } = data;
+
+          return res.status(StatusCodes.OK).json({ id });
+        } else {
+          if (timeDiffInMinutes(verificationData.lastSentEmailTime) < +process.env.COOLDOWN_TIME) {
+            const blockSecondsLeft = Math.round(
+              60 - (new Date().getTime() - verificationData.lastSentEmailTime.getTime()) / 1000
+            );
+  
+            return res.status(StatusCodes.NOT_ACCEPTABLE).json({ blockSeconds: blockSecondsLeft, msg: messages.COOLDOWN });
+          }
+  
+          const timeObj = generateTime();
+          const data = await this.securityService.sendCode(
+            email,
+            timeObj.codeExpirationTime,
+            timeObj.lastSentEmailTime
+          );
+
+          return res.status(StatusCodes.OK).json({ id: data.id, email: data.email });
+        }
+      } else {
+        return res.status(StatusCodes.CONFLICT).json({ msg: messages.USER_NOT_FOUND });
+      }
+    } catch (error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: messages.INTERNAL_SERVER_ERROR });
+    }
+  }
+ 
   public sendVerificationCode = async (req: Request, res: Response) => {
     try {
       const { email } = plainToInstance(EmailDto, req.body);
@@ -35,9 +103,8 @@ export default class SecurityController {
           timeObj.lastSentEmailTime
         );
         const { id } = data;
-        const blockSecondsLeft = Math.round(60 - (new Date().getTime() - timeObj.lastSentEmailTime.getTime()) / 1000);
 
-        return res.status(StatusCodes.OK).json({ id, blockSeconds: blockSecondsLeft });
+        return res.status(StatusCodes.OK).json({ id });
       } else {
         if (timeDiffInMinutes(verificationData.lastSentEmailTime) < +process.env.COOLDOWN_TIME) {
           const blockSecondsLeft = Math.round(
