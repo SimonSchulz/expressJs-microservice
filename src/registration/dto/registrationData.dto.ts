51ef9fc8940de35a0df4cd38caaaef1de63bc2f0
ivanlyutov17 <lyutov17@yandex.ru>
2022-12-09 10:58:19 +0100

    changed validation properties

--- src/registration/dto/registrationData.dto.ts
+++ src/registration/dto/registrationData.dto.ts
@@ -19,7 +19,7 @@ export class RegistrationDataDto {
 
   @IsDefined()
   @IsString()
-  @Matches(RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/))
+  @Matches(RegExp(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g))
   password: string;
 
   @IsString()
--- src/registration/dto/updateData.dto.ts
+++ src/registration/dto/updateData.dto.ts
@@ -9,7 +9,7 @@ export default class UpdateUserProfileDto {
 
   @IsDefined()
   @IsString()
-  @Matches(RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/))
+  @Matches(RegExp(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g))
   password: string;
 
   @IsDefined()
