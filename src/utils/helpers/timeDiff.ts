313079bbc56c8c63d131fbae867c094f11fa2ecd
Pavel Zharski <p.zharski@gmail.com>
2023-01-30 10:22:48 +0100

    Added better validation for time

--- src/utils/helpers/timeDiff.ts
+++ src/utils/helpers/timeDiff.ts
@@ -2,3 +2,8 @@ export const timeDiffInMinutes = (prevTime) => {
   const currentTime = Date.now();
   return Math.round(new Date(currentTime).getTime() - new Date(prevTime).getTime()) / 1000 / 60;
 };
+
+export const timeDiffInHours = (prevTime) => {
+  const currentTime = Date.now();
+  return Math.round((new Date(currentTime).getTime() - new Date(prevTime).getTime()) / 1000 / 60 / 60);
+}
